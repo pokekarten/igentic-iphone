24d000b32bf422eab59c5e287bf4b7ec973ab43d
pokekarten <benjamin.panahi.muc@gmail.com>
2026-06-12 18:59:32 +0300

    feat: add approval manager stub

---
+++ ios/Sources/AgentCore/ApprovalManager.swift
@@ -0,0 +1,39 @@
+import Foundation
+
+public enum ApprovalStatus: String, Equatable, Sendable {
+    case notRequired
+    case approved
+    case pending
+    case rejected
+}
+
+public struct ApprovalRequest: Equatable, Sendable {
+    public let taskSummary: String
+    public let dataClassification: DataClassification
+    public let actionRisk: ActionRisk
+    public let reason: String
+
+    public init(
+        taskSummary: String,
+        dataClassification: DataClassification,
+        actionRisk: ActionRisk,
+        reason: String
+    ) {
+        self.taskSummary = taskSummary
+        self.dataClassification = dataClassification
+        self.actionRisk = actionRisk
+        self.reason = reason
+    }
+}
+
+public struct ApprovalManager: Sendable {
+    private let defaultStatus: ApprovalStatus
+
+    public init(defaultStatus: ApprovalStatus = .pending) {
+        self.defaultStatus = defaultStatus
+    }
+
+    public func requestApproval(_ request: ApprovalRequest) -> ApprovalStatus {
+        defaultStatus
+    }
+}
