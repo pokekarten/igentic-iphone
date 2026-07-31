2a881562357244abe7f2384ebd795cad9548322d
pokekarten <benjamin.panahi.muc@gmail.com>
2026-07-31 10:10:57 +0200

    Bootstrap the local app action approval policy at app startup

--- app/iGenticDiagnostics/iGenticDiagnostics/iGenticDiagnosticsApp.swift
+++ app/iGenticDiagnostics/iGenticDiagnostics/iGenticDiagnosticsApp.swift
@@ -1,8 +1,14 @@
 import SwiftUI
+import AgentCore
 import iGenticApp
 
 @main
 struct iGenticDiagnosticsApp: App {
+    init() {
+        let store = AppActionApprovalPolicyStore(fileURL: AppActionApprovalPolicyStore.defaultFileURL())
+        _ = try? store.loadOrInstallDefault()
+    }
+
     var body: some Scene {
         WindowGroup {
             DiagnosticView()
